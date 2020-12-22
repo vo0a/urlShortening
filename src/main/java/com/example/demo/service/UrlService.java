9600d5889c9f94eaa8c48570eb0e28c832db8d31
daromKim <vo0a195@gmail.com>
2020-12-22 22:20:12 +0900

    refactor : static한 데이터 반환

--- src/main/java/com/example/demo/service/UrlService.java
+++ src/main/java/com/example/demo/service/UrlService.java
@@ -5,20 +5,28 @@ import com.example.demo.domain.UrlRepository;
 import com.example.demo.web.dto.UrlRequestDto;
 import com.example.demo.web.dto.UrlResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Base64Utils;
+import sun.misc.BASE64Encoder;
 
 @RequiredArgsConstructor
 @Service
 public class UrlService {
 
     private final UrlRepository urlRepository;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
     public UrlResponseDto save(UrlRequestDto requestDto){
 
-        // print original Url
-        System.out.println(requestDto.getOriginalUrl());
+        // 중복관점 - read먼저해서 있으면 돌려주고 없으면 쓰기
+        // 캐시와 마스터 에 있는지 먼저 확인.
 
+        // print original Url
+        logger.info(requestDto.getOriginalUrl());
+
+        // 더 짧게 만들기
         String shortenUrl = Base64Utils.encodeToString(requestDto.getOriginalUrl().getBytes());
 
         // print shorten Url
@@ -29,7 +37,12 @@ public class UrlService {
                 .shortenUrl(shortenUrl)
                 .build();
 
-        return new UrlResponseDto(urlRepository.save(entity));
+        // try catch 결과 확인 예외처리.
+        // 중복 예외처리 - 없을 때만 쓰기
+        urlRepository.save(entity);
+        UrlResponseDto ret = findByShortenUrl(shortenUrl);
+
+        return ret;
     }
 
 
--- src/main/java/com/example/demo/web/dto/UrlRequestDto.java
+++ src/main/java/com/example/demo/web/dto/UrlRequestDto.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class UrlRequestDto {
+    // "originalUrl" : "http://" - json sequlize
     private String originalUrl;
 
     @Builder
